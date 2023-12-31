961a0dc907bee93ff57e6f46a78be2c54fd8bf76
sajid ali <sbhattti1212@gmail.com>
2023-12-31 17:23:43 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyComponent from "./components/Test";
+import { useAuth } from "./context/global";
+import Dashbaord from "./components/Dashbaord";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
+import { Routes, Route } from "react-router";
+import { NavLink } from "react-router-dom";
 
 function App() {
+  const {
+    isLoggedIn,
+    setIsLoggedIn,
+    getToken,
+    setGetToken,
+    userData,
+    setUserData,
+    login,
+    logout,
+  } = useAuth();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent />
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashbaord />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
